df59eb1492dd7066389b2c7829051b8c527e89e8
briossant <crosson.brieuc@gmail.com>
2021-12-05 22:27:58 +0100

    day 4

--- days/day04.js
+++ days/day04.js
@@ -2,20 +2,30 @@
 exports.run = function (input) {
     input = input.split('\n');
 
-    const numbers = input[0].split(',');
+    const numbers = input[0].replace('\r','').split(',');
     const grids = [[]];
     let winning_grid;
     let winning_num = -1;
 
     let i2 = 0;
     for (let i = 2; i < input.length; i++) {
-        if(input[i] === ''){
+        if(input[i] === '' || input[i] === '\r'){
             i2++;
             grids.push([]);
             continue;
         }
-        grids[i2].push(input[i].split(' ').filter(x=>x!==''));
+        grids[i2].push(input[i].replace('\r','').split(' ').filter(x=>x!==''));
     }
+
+
+    function checkCollums(ls, i){
+        let sm = 0;
+        ls.forEach(s=>{
+            sm+=s[i];
+        })
+        return sm === -5
+    }
+
 
     numbers.forEach(num=> {
         const to_rm = [];
@@ -23,9 +33,9 @@ exports.run = function (input) {
             for (let y = 0; y < grids[x].length; y++) {
                 const index = grids[x][y].indexOf(num);
                 if(index > -1){
-                    grids[x][y].splice(index,1);
+                    grids[x][y][index] = -1;
                 }
-                if(grids[x][y].length === 0){
+                if(grids[x][y].reduce((a,b)=>a+b) === -5 || checkCollums(grids[x],index)){
                     winning_grid = grids[x];
                     winning_num = num;
                     to_rm.push(x);
@@ -33,6 +43,8 @@ exports.run = function (input) {
                 }
             }
         }
+        to_rm.reverse();
+
         to_rm.forEach(x=>{
             grids.splice(x,1);
         });
@@ -42,10 +54,12 @@ exports.run = function (input) {
 
     for (let x = 0; x < winning_grid.length; x++) {
         for (let y = 0; y < winning_grid[x].length; y++) {
-            sum += ~~winning_grid[x][y];
+            if(winning_grid[x][y] !== -1) {
+                sum += ~~winning_grid[x][y];
+            }
         }
     }
+    console.log(winning_grid, sum, winning_num)
 
-    console.log(winning_grid)
     return sum * winning_num;
 }
--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const path = require('path');
 
 
 const day = '04';
-
+const test_input = false;
 
 
 
 const d = require(`./days/day${day}`);
-console.log(d.run(fs.readFileSync(path.resolve(__dirname, `./inputs/day${day}.txt`), 'utf8')));
+console.log(d.run(fs.readFileSync(path.resolve(__dirname, `./${test_input ? 'test_' : ''}inputs/day${day}.txt`), 'utf8')));
